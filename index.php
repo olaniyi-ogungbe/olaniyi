2feeb628e9966a374d2dda79fd0cd55905277268
olaniyi-ogungbe <38250438+olaniyi-ogungbe@users.noreply.github.com>
2018-04-10 18:47:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>HNG Stage 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+    body {
+    
+    background: url(https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg) no-repeat;
+    background-size: cover;
+    color: cyan;
+    padding: 12em;
+    h1 {
+    font-size: 3em;
+    color: #FF9830;
+}
+#time {
+    color: black;
+</style>
+</head>
+<body>
+	<main>
+        <header style="text-align: center;">
+            <h1>Hello World<h1>
+        </header>
+        <section>
+            <br>
+            <br>
+            <p >
+            	<ul>
+            		<li>Design a page in Figma. </li>
+            			<li>Implement in HTML/CSS. </li>
+            				<li>Setup docker. </li>
+            	</ul>
+             Await further instructions.</p>
+              <p>Message received at</p>
+            <p><?php echo date("l j F Y h:i A"); ?></p>
+        </section>
+</main>
+</body>        
